434bcf0d3c05bacb8c3a69e7e5808b75533a038d
Kay <166387418+unspokenrules@users.noreply.github.com>
2025-06-24 13:29:54 -0400

    Update core.py

--- drifter/core.py
+++ drifter/core.py
@@ -1,1 +1,19 @@
 
+def compare_resources(tf_resources, gcp_resources):
+    """
+    Compare Terraform resources and live GCP resources.
+    Returns a dictionary of unmanaged, stale, and managed resources.
+    """
+    tf_set = set((res['type'], res['name']) for res in tf_resources)
+    gcp_set = set((res['type'], res['name']) for res in gcp_resources)
+
+    unmanaged = gcp_set - tf_set
+    stale = tf_set - gcp_set
+    managed = tf_set & gcp_set
+
+    return {
+        "unmanaged": sorted(unmanaged),
+        "stale": sorted(stale),
+        "managed": sorted(managed)
+    }
+
