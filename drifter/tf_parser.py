c27ec2d504d013771f554807e96921ff92d91592
Kay <166387418+unspokenrules@users.noreply.github.com>
2025-06-24 13:31:05 -0400

    Update tf_parser.py

--- drifter/tf_parser.py
+++ drifter/tf_parser.py
@@ -1,1 +1,40 @@
+import json
 
+def load_terraform_state(state_file_path):
+    """
+    Loads and parses Terraform state JSON file.
+    Returns a list of normalized resource dicts.
+    """
+    with open(state_file_path, "r") as f:
+        data = json.load(f)
+
+    resources = []
+
+    for res in data.get("values", {}).get("root_module", {}).get("resources", []):
+        res_type = res.get("type")
+        res_name = res.get("name")
+        res_values = res.get("values", {})
+
+        resource_entry = {
+            "type": f"google_{res_type}",
+            "name": res_name,
+            "id": res_values.get("id", ""),
+            "attributes": res_values
+        }
+        resources.append(resource_entry)
+
+    for mod in data.get("values", {}).get("root_module", {}).get("child_modules", []):
+        for res in mod.get("resources", []):
+            res_type = res.get("type")
+            res_name = res.get("name")
+            res_values = res.get("values", {})
+
+            resource_entry = {
+                "type": f"google_{res_type}",
+                "name": res_name,
+                "id": res_values.get("id", ""),
+                "attributes": res_values
+            }
+            resources.append(resource_entry)
+
+    return resources
