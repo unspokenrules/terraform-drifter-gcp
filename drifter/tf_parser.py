f629b109b30798a4c545dbcac0d68b8c6a8c185d
Kay <166387418+unspokenrules@users.noreply.github.com>
2025-06-24 14:52:42 -0400

    Update tf_parser.py

--- drifter/tf_parser.py
+++ drifter/tf_parser.py
@@ -1,40 +1,32 @@
-import json
-
+# drifter/tf_parser.py
 def load_terraform_state(state_file_path):
-    """
-    Loads and parses Terraform state JSON file.
-    Returns a list of normalized resource dicts.
-    """
+    import json
     with open(state_file_path, "r") as f:
         data = json.load(f)
 
     resources = []
 
-    for res in data.get("values", {}).get("root_module", {}).get("resources", []):
-        res_type = res.get("type")
-        res_name = res.get("name")
-        res_values = res.get("values", {})
-
-        resource_entry = {
-            "type": f"google_{res_type}",
-            "name": res_name,
-            "id": res_values.get("id", ""),
-            "attributes": res_values
-        }
-        resources.append(resource_entry)
-
-    for mod in data.get("values", {}).get("root_module", {}).get("child_modules", []):
+    def extract_resources(mod):
         for res in mod.get("resources", []):
             res_type = res.get("type")
             res_name = res.get("name")
             res_values = res.get("values", {})
 
-            resource_entry = {
-                "type": f"google_{res_type}",
-                "name": res_name,
-                "id": res_values.get("id", ""),
-                "attributes": res_values
-            }
-            resources.append(resource_entry)
+            gcp_resource_type = {
+                "google_compute_instance": "compute.googleapis.com/Instance",
+                "google_project_iam_custom_role": "iam.googleapis.com/Role"
+            }.get(res_type)
+
+            if gcp_resource_type:
+                resources.append({
+                    "type": gcp_resource_type,
+                    "name": res_values.get("role_id", res_name),  # role_id is key for IAM roles
+                    "attributes": res_values
+                })
+
+    root = data.get("values", {}).get("root_module", {})
+    extract_resources(root)
+    for mod in root.get("child_modules", []):
+        extract_resources(mod)
 
     return resources
